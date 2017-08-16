7327d2637382779b0fd7950c2e3a166bbd21a54a
ruiliang <liangrui_1988@126.com>
2017-08-16 20:01:02 +0800

    pg test

--- src/main/java/com/client/kafka/PGsqlTest.java
+++ src/main/java/com/client/kafka/PGsqlTest.java
@@ -1,7 +1,6 @@
 package com.client.kafka;
 
 import java.util.Arrays;
-import java.util.Date;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -13,15 +12,10 @@ import org.junit.Test;
 import com.client.kafka.builder.ConsumerBuilder;
 import com.client.kafka.builder.ProducerBuilder;
 
-public class BuilderTest {
+public class PGsqlTest {
 
-	@Test
-	public void producerUtil() {
-		System.out.println("发送kafka........");
-		Producer<String, String> producer3 = new ProducerBuilder().kafkaProducerBuilder();
-		producer3.send(new ProducerRecord<String, String>("foo", "k-1", "msg-foo-v" + new Date()));
-		producer3.send(new ProducerRecord<String, String>("bar", "k-2", "msg-bar-v" + new Date()));
-		producer3.close();
+	public PGsqlTest() {
+		// TODO Auto-generated constructor stub
 	}
 
 	@Test
@@ -29,35 +23,17 @@ public class BuilderTest {
 		System.out.println("发送kafka........");
 		Producer<String, String> producer3 = new ProducerBuilder().kafkaProducerBuilder();
 		for (int i = 0; i < 1000000; i++) {
-			String json="{\"file\":\"tk1\",\"size\":\"test\"}";
+			String json = "{\"file\":\"tk1\",\"size\":\"test\"}";
 			producer3.send(new ProducerRecord<String, String>("logtest", "k-1", json));
 		}
 		producer3.close();
 	}
 
 	@Test
-	public void consumerTest() {
-		System.out.println("接收kafka........");
-		KafkaConsumer<String, String> consumer = new ConsumerBuilder().groupId("test").kafkaConsumerBuilder();
-		consumer.subscribe(Arrays.asList("foo", "bar"));
-		while (true) {
-			ConsumerRecords<String, String> records = consumer.poll(100);// “long
-																			// poll”进行阻塞，直到数据到达（并且设置等待时间的好处是可以积累消息，组成大数据块一并发送）。
-			int i = 0;
-			for (ConsumerRecord<String, String> record : records) {
-				i++;
-				System.out.printf(i + ":offset = %d, key = %s, value = %s%n", record.offset(), record.key(),
-						record.value());
-
-			}
-		}
-	}
-
-	@Test
 	public void consumerTest2() {
 		System.out.println("接收kafka Group2........");
 		KafkaConsumer<String, String> consumer = new ConsumerBuilder().groupId("test3").kafkaConsumerBuilder();
-		consumer.subscribe(Arrays.asList("foo", "bar"));
+		consumer.subscribe(Arrays.asList("gamelog"));
 		while (true) {
 			ConsumerRecords<String, String> records = consumer.poll(100);// “long
 																			// poll”进行阻塞，直到数据到达（并且设置等待时间的好处是可以积累消息，组成大数据块一并发送）。
